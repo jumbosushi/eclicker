080de03237444d8a8ab6b3b8a2835346f46b48bb
TuOfTimes <edtuwork@gmail.com>
2017-01-29 10:12:24 -0500

    added page2

---
+++ src/page2/page2.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './page1.css';
+class Page1 extends Component {
+  render() {
+    return (
+
+      <div className="page1">
+        <div className="top-wrap">
+          <div className="top">
+            <h1> Create </h1>
+          </div>
+        </div>
+
+        <div className="bottom-wrap">
+          <div className="bottom">
+            <h1> Enter </h1>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Page1;
